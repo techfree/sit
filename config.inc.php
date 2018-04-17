7b34f0b75ebc76ab565af6a36edad1cb20225a89
Ouedarbi mohamed <techfree1081@gmail.com>
2018-04-17 15:04:45 +0200

    First Commit

---
+++ config.inc.php
@@ -0,0 +1,18 @@
+<?php
+# config.inc.php - SiT! Config file generated automatically by setup.php on Mon, 16 Apr 2018 15:49:14 +0200
+
+# Web Path
+# The path to SiT! from the browsers perspective with a trailing slash. e.g. /sit/
+$CONFIG['application_webpath'] = '/sit-3.67/';
+
+# MySQL Database Username
+$CONFIG['db_username'] = 'root';
+
+# MySQL Database Table Prefix
+# Optionally prefix database table names with the a string (e.g. 'sit_', use this if the database you are using is shared with other applications
+$CONFIG['db_tableprefix'] = 'sit_';
+
+# Attachment Filesystem Path
+# The full absolute file system path to a directory to store attachments in (with a trailing slash). This directory should be writable
+$CONFIG['attachment_fspath'] = 'E:\\Xampp\\htdocs\\sit-3.67\\attachments-c7cd5cf8fb69932f8dc42f3d421ba9b9aa0f5a96\\';
+
